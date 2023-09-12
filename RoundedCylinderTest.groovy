55d6acfa9f45fd81d3db56818a8838f58e42d019
Maura Hunter <Maura Hunter@192.168.21.71>
2023-09-12 17:27:26 -0400

    I changed the roundcyliner numbers

--- RoundedCylinderTest.groovy
+++ RoundedCylinderTest.groovy
@@ -1,5 +1,5 @@
 
-CSG roundedCylinder = new RoundedCylinder(50,60.0)
+CSG roundedCylinder = new RoundedCylinder(100,160.0)
                                 .cornerRadius(10)// sets the radius of the corner
                                 .toCSG()// converts it to a CSG tor display
 
